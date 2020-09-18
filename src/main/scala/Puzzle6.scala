7edd3952577cdc2e9ee55095fbe52868b19fa003
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-18 10:46:55 +0900

    feat: add functions

--- src/main/scala/Puzzle6.scala
+++ src/main/scala/Puzzle6.scala
@@ -1,3 +1,13 @@
 object Puzzle6 {
-  def output(str: String): Unit = println(s"$str")
+  // Q1
+  def applyNMulti[T](n: Int)(arg: T, f: T => T): T =
+    (1 to n).foldLeft(arg) {(acc, _) => f(acc) }
+
+  def applyNCurrried[T](n: Int)(arg: T)(f: T => T): T =
+    (1 to n).foldLeft(arg) { (acc, _) => f(acc) }
+
+  def nextInt(n: Int): Int = n * n + 1
+
+  def nextNumber[N](n: N)(implicit numericOps: Numeric[N]): N =
+    numericOps.plus(numericOps.times(n, n), numericOps.one)
 }
