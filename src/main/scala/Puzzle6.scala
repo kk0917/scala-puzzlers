e75a8c6052729b1963f17a74f027b11ff2c699a7
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-18 11:08:19 +0900

    fix: add space readable

--- src/main/scala/Puzzle6.scala
+++ src/main/scala/Puzzle6.scala
@@ -1,7 +1,7 @@
 object Puzzle6 {
   // Q1
   def applyNMulti[T](n: Int)(arg: T, f: T => T): T =
-    (1 to n).foldLeft(arg) {(acc, _) => f(acc) }
+    (1 to n).foldLeft(arg) { (acc, _) => f(acc) }
 
   def applyNCurrried[T](n: Int)(arg: T)(f: T => T): T =
     (1 to n).foldLeft(arg) { (acc, _) => f(acc) }
@@ -10,4 +10,20 @@ object Puzzle6 {
 
   def nextNumber[N](n: N)(implicit numericOps: Numeric[N]): N =
     numericOps.plus(numericOps.times(n, n), numericOps.one)
+  /**
+   * scala> println(Puzzle6.applyNMulti(3)(2, Puzzle6.nextInt))
+   * 677
+   *
+   * scala> println(Puzzle6.applyNCurrried(3)(2)(Puzzle6.nextInt))
+   * 677
+   *
+   * scala> println(Puzzle6.applyNMulti(3)(2.0, Puzzle6.nextInt))
+   * error: type mismatch;
+   * found   : Int => Int
+   * required: AnyVal => AnyVal
+   *
+   * scala> println(Puzzle6.applyNCurrried(3)(2.0)(Puzzle6.nextNumber))
+   * 677.0
+   *
+   **/
 }
