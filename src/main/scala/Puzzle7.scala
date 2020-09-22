c611efb99077a4d06fba8debfa3aaa35362346cb
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-22 12:12:20 +0900

    learn how to use safe variable into closure

--- src/main/scala/Puzzle7.scala
+++ src/main/scala/Puzzle7.scala
@@ -11,7 +11,41 @@ object Puzzle7 {
     accessors2 += (() => data(j))
     j += 1
   }
+  /**
+   * scala> Puzzle7.accessors1.foreach(a1 => println(a1()))
+   * 100
+   * 110
+   * 120
+   *
+   * scala> Puzzle7.accessors2.foreach(a2 => println(a2()))
+   * Puzzle7.accessors2.foreach(a2 => println(a2()))java.lang.IndexOutOfBoundsException: 3
+   * at scala.collection.LinearSeqOps.apply(LinearSeq.scala:116)
+   * ...
+   */
+
+  /**
+   * var j is mutable, and so its variable change value
+   * def fun(): Function0 = {
+   *   val i: Int = 1
+   *   var j: runtime.IntRef = new runtime.IntRef(2)
+   *   {
+   *     (new anonymous class anonfunfun(Illustration.this, i, j): Function0)
+   *   }
+   * }
+   */
+  /**
+   * if you should use variables, it assign var to val
+   * ...
+   * for (i <- 0 until data.length) {
+   *   val currentJ = j
+   *   accessors2 += (() => data(currentJ))
+   *   j += 1
+   * }
+   * ...
+   *
+   * scala> Puzzle7.accessors2.foreach(a2 => println(a2()))
+   * 100
+   * 110
+   * 120
+   */
 }
-/**
- * scala>
- */
