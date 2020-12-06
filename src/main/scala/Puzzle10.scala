02c32491c778a8003efa6dcf47aa5c6208913cb6
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-06 22:34:51 +0900

    Puzzle10
    Learn how to use case class that have various utility method

--- src/main/scala/Puzzle10.scala
+++ src/main/scala/Puzzle10.scala
@@ -1,9 +1,14 @@
 object Puzzle10 {
+  class Country(val isoCode: String, val name: String)
+  case class CountryCC(isoCode: String, name: String)
 
+  val homeOfScala   = new Country("CH", "Switzerland")
+  val homeOfScalaCC = CountryCC("CH", "Switzerland")
+  /**
+   * > println(Puzzle10.homeOfScala equals new Country("CH", "Switzerland"))
+   * false
+   *
+   * > println(Puzzle10.homeOfScalaCC equals CountryCC("CH", "Switzerland"))
+   * false // expected true...
+   */
 }
-
-class Country(val isoCode: String, val name: String)
-case class CountryCC(isoCode: String, name: String)
-
-val homeOfScala   = new Country("CH", "Switzerland")
-val homeOfScalaCC = CountryCC("CH", "switzer")
