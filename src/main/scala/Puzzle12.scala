afff1134e1c11782a87354e1407fa723790d6a29
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-15 21:18:18 +0900

    Learn careful point when converting the order of Collection type.
    Puzzle12

--- src/main/scala/Puzzle12.scala
+++ src/main/scala/Puzzle12.scala
@@ -19,7 +19,38 @@ object Puzzle12 {
 
   println("Roman numeral symbols for 1 5 10 50 100 500 1000:")
 
-  for (num <- numerals; sym = num.symbol) { print(s"${sym}") }
+  for (num <- numerals; sym = num.symbol) { print(s"${sym}") } // IVXLCDM
 
-  numerals map { _.symbol } foreach { sym => print(s"${sym}") }
+  println("")
+
+  numerals map { _.symbol } foreach { sym => print(s"${sym}") } // CDILMVX
+
+  println("")
+
+  // Consideration
+  for (num <- numerals) print(s"${num.symbol}")
+
+  println("")
+
+  numerals foreach { num => print(s"${num.symbol}") }
+
+  println("")
+
+  // You can use Collection's View as more general purpose alternative
+  numerals.view map { _.symbol } foreach { sym => print(s"${sym}") }
+
+  println("")
+
+  /**
+   * Most simple alternative is
+   *   to use the Collection type that doesn't change by converting like Seq type
+   */
+  numerals.toSeq map { _.symbol } foreach { sym => print(s"${sym}") }
+
+  println("")
+
+  /** Summary
+   * Be careful not to be taken over the source collection's order automatically
+   *   when the converting execute on Collection type, especially when let several operations chained.
+   */
 }
