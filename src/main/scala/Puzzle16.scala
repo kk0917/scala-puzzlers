ad0b4fdd539714d8cc98c9235f3becdb6bafba15
ko-kamenashi <kamenashi0917@gmail.com>
2021-01-07 21:07:51 +0900

    add sample
    puzzle16

--- src/main/scala/Puzzle16.scala
+++ src/main/scala/Puzzle16.scala
@@ -3,15 +3,15 @@ object Puzzle16 {
   def regular(x: Int, y: Int, z: Int): Int = x + y + z // 6
   def curried(x: Int)(y: Int)(z: Int): Int = x + y + z // 6
 
-  def invert(v3: Int)(v2: Int = 2, v1: Int = 1) = {
+  def invert(v3: Int)(v2: Int = 2, v1: Int = 1): Unit = {
     println(v1 + ", " + v2 + ", " + v3)
   }
 
-  val invert3 = invert(3) _
+  val invert3: (Int, Int) => Unit = invert(3) _
 
   invert3(v1 = 2)
   /**
-   * > invert3(v3 = 2)
+   * > invert3(v1 = 2)
    * error: not enough arguments for method apply: (v1: Int, v2: Int): Unit in trait Function2.
    * Unspecified value parameter v2.
    **/
@@ -22,5 +22,16 @@ object Puzzle16 {
    * 1, 2, 3 // Be outputted line up as defined in the definition ordering.
    */
 
-  // To be continued...
+  def foldLeft[B](z: B)(op: (B, A) => B): B
+  /**
+   * > Seq("I", "II", "III").foldLeft(0)(_ + _.length)
+   * res0: Int = 6
+   */
+
+  /** Summery
+   * Remember that the named arguments are resolved by generated function object,
+   *   not by source method when calling part-applied functions.
+   *
+   * Be able to avoid this problem by using arguments name used at Scala's function trait.
+   */
 }
