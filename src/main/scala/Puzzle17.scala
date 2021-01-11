8d0259f542a945079cff04ff14645e2e0c38824c
ko-kamenashi <kamenashi0917@gmail.com>
2021-01-11 14:26:45 +0900

    add sample
    puzzle17

--- src/main/scala/Puzzle17.scala
+++ src/main/scala/Puzzle17.scala
@@ -7,12 +7,12 @@ object Puzzle17 {
    * val res0: Int = 5
    */
 
-  implicit val z1: Int = 2
-  def add(x: Int)(y: Int)(implicit z: Int): Int = x + y + z
-  def addTo(n: Int): Int => Int = add(n)
-
-  implicit val z2: Int = 3
-  val addTo1: Int => Int = addTo(1)
+//  implicit val z1: Int = 2
+//  def add(x: Int)(y: Int)(implicit z: Int): Int = x + y + z
+//  def addTo(n: Int): Int => Int = add(n)
+//
+//  implicit val z2: Int = 3
+//  val addTo1: Int => Int = addTo(1)
 
   /**
    * > addTo1(2)
@@ -22,4 +22,30 @@ object Puzzle17 {
    *            ^
    * error: Int does not take parameters
    **/
+
+  /** Anonymous Function and implicit parameters
+   *
+   */
+  def iNeedAnImplicit(implicit n: Int): Int = n + 1
+  /**
+   * > val anonFunc = { x: Int => y: Int => x + y + Puzzle17.iNeedAnImplicit }
+   *                                                         ^
+   * error: could not find implicit value for parameter n: Int
+   */
+
+  val anonFuncWithImplicitParam = { x: Int => implicit y: Int => x + y + Puzzle17.iNeedAnImplicit }
+
+  implicit val z = 2
+  /**
+   * > val anonFuncWithImplicitParam = { x: Int => implicit y: Int => x + y + Puzzle17.iNeedAnImplicit }
+   * val anonFuncWithImplicitParam: Int => (Int => Int) = "< function1 >"
+   *
+   * > anonFuncWithImplicitParam(1)(2)
+   * val res0: Int = 6
+   *
+   * // compiler doesn't supply the implicit parameters
+   * > anonFuncWithImplicitParam(1)
+   * val res1: Int => Int = < function2>
+   */
+
 }
