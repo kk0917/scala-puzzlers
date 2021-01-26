fa70f5486b507ff3b12be4c1e5346ea1be5a3d6d
ko-kamenashi <kamenashi0917@gmail.com>
2021-01-26 22:12:03 +0900

    refactor: added the cases of StringBuffer
    Puzzle21

--- src/main/scala/Puzzle21.scala
+++ src/main/scala/Puzzle21.scala
@@ -4,18 +4,35 @@ object Puzzle21 {
       1 to width - sb.length foreach { sb +=  '*'  }
       sb
     }
+
+    def eqPad2(width: Int): StringBuilder = {
+      val appendedSb = sb += '*'
+      1 to width - sb.length foreach appendedSb.apply
+      sb
+    }
+
+    def refactorPad2(width: Int): StringBuilder = {
+      1 to width - sb.length foreach { _ => sb += '*' }
+      /**
+       * if using for brace like this, be expanded to the above code
+       *   -> for (_ <- 1 to width = sb.length) { sb += '*' }
+       */
+
+      sb
+    }
   }
 
   val greeting = new StringBuilder("Hello, kitteh!")
   println(greeting pad2 20)
+  // Hello, kitteh!*
 
   val farewell = new StringBuilder("U go now.")
   println(farewell pad2 20)
-}
+  // java.lang.StringIndexOutOfBoundsException: ...
 
-class Adder(val sb: StringBuilder) {
-  def add(width: Int): StringBuilder = {
-    1 to width - sb.length foreach { sb +=  '*'  }
-    sb
-  }
+  println(new StringBuilder("Hello, kitteh!").padTo(20,'*').mkString)
+  // Hello, kitteh!******
+
+  println(new StringBuilder("U go now.").padTo(20, '*').mkString)
+  // U go now.***********
 }
